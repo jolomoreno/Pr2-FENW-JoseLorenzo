827832bb0d9fc10b0def0d9eacaa9d42b78a4d71
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-17 01:13:57 +0100

    Estructura básica del proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { MasterComponent } from './layouts/master/master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    FooterComponent,
+    HeaderComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MasterComponent]
 })
 export class AppModule { }
