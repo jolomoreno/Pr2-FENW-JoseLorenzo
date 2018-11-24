61056f7b1c68821e4a01247b4974bc01c68436b3
Jose Lorenzo <jolorenzom@gmail.com>
2018-11-24 22:55:08 +0100

    Form Registro reactivo (queda el servicio de conexión con API)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FooterComponent } from './components/root/modules/footer/footer.component';
-import { HeaderComponent } from './components/root/modules/header/header.component';
+import { FooterComponent } from './root/components/footer/footer.component';
+import { HeaderComponent } from './root/components/header/header.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
 import { HomeComponent } from './components/home/home.component';
 import { InstalacionesComponent } from './components/instalaciones/instalaciones.component';
-import { RootComponent } from './components/root/root.component';
+import { RootComponent } from './root/root.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
 
@@ -25,7 +25,8 @@ import { RegistroComponent } from './components/registro/registro.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [RootComponent]
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/user';
-import { NgForm } from '@angular/forms';
+import { User } from '../../shared/models/user';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,24 +9,71 @@ import { NgForm } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
   public user: User;
+  usernameDuplicated = false;
+  passwordsDuplicated = true;
+
+  formRegistro = new FormGroup({
+    username: new FormControl('',
+      [Validators.required]),
+    password: new FormControl('',
+      [Validators.required]),
+    passwordRepeated: new FormControl('',
+      [Validators.required]),
+    email: new FormControl('',
+      [Validators.required, Validators.email]),
+    birthdate: new FormControl('2018-11-24')
+  });
 
   constructor() {
     this.user = new User('', '', '', '', 0);
   }
 
-  ngOnInit() {
-  }
+  get f() { return this.formRegistro.controls; }
 
-  onSubmit(formRegistro: NgForm) {
+  ngOnInit() {}
+
+  onSubmit(formRegistroSubmitted: FormGroup) {
     console.log('Has pulsado el botón de ENVIAR');
-    console.log(this.user, ' <<<< Usuario');
-    formRegistro.reset();
+    this.user.username = formRegistroSubmitted.get('username').value;
+    this.user.password = formRegistroSubmitted.get('password').value;
+    this.user.passwordRepeated = formRegistroSubmitted.get('passwordRepeated').value;
+    this.user.email = formRegistroSubmitted.get('email').value;
+    this.user.birthdate = formRegistroSubmitted.get('birthdate').value;
+
+    /*console.log(this.user.username, 'USERNAME-SUBMIT');
+    console.log(this.user.password, 'PASSWORD-SUBMIT');
+    console.log(this.user.passwordRepeated, 'PASSREPEATED-SUBMIT');
+    console.log(this.user.email, 'EMAIL-SUBMIT');
+    console.log(this.user.birthdate, 'BIRTHDATE-SUBMIT');*/
+
+    this.checkPasswords(this.user.password, this.user.passwordRepeated, formRegistroSubmitted);
   }
 
-  checkUsername() {
-    if (this.user.username !== '' && this.user.username !== undefined) {
-      console.log(this.user.username, '<<< USERNAME');
-      // Hacer llamada a API para comprobar que no hay username repetidos en DB
+  checkUsername(formRegistro) {
+    /*console.log('Has hecho BLUR del input de USER');
+    console.log(formRegistro.get('username').value, 'USERNAME-BLUR');*/
+    // Buscar el username en DB
+    if (formRegistro.get('username').value === 'Paco') {
+      console.log('SON IGUALES!!!');
+      this.usernameDuplicated = true;
+    } else {
+      console.log('NO SON IGUALES!!!');
+      this.usernameDuplicated = false;
     }
+  }
+
+  checkPasswords(password: string, passwordRepeated: string, formRegistroSubmitted: FormGroup) {
+    console.log(password, 'PASSWORD-SUBMIT');
+    console.log(passwordRepeated, 'PASSREPEATED-SUBMIT');
+    if (password === passwordRepeated) {
+      console.log('SON IGUALES!!!');
+      // formRegistroSubmitted.reset();
+      this.passwordsDuplicated = true;
+    } else {
+      console.log('NO SON IGUALES');
+      this.passwordsDuplicated = false;
+    }
+    console.log(this.passwordsDuplicated, 'passwordsDuplicated');
+
   }
 }
